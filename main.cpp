5b6b69bbf818f1437089abf00226922629403220
keresan <martin.mchlk@gmail.com>
2014-05-29 12:39:32 +0200

    vyvoj verzie 2

--- main.cpp
+++ main.cpp
@@ -13,17 +13,10 @@
  *
  *
  */
-
-
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-
-
 
     return a.exec();
 }
